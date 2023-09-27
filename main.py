3e58bd19a0948374e8e7702cf43ee868fd3b35da
Uczeń <jarek.w.wojcik@gmail.com>
2023-09-27 14:58:36 +0200

    nubnjbhjbhj

---
+++ main.py
@@ -0,0 +1,42 @@
+
+
+liczba1 = 8
+print(type(liczba1))
+
+tekst1 = "informatyka"
+print(type(liczba1))
+
+log1 = True
+print(type(log1))
+
+liczba1 = "xd"
+print(type(liczba1))
+
+print(liczba1)
+
+liczba2 = 2.67
+print(type(liczba2))
+
+lista1 = ["kot", 12, False, 3.89]
+print(lista1)
+
+zbior1 = {1,1,1,1,2,3,4,1,1,1,5,5,5,4}
+print(type(zbior1))
+
+dictionary1 = {"kot" : "spada na cztery lapy" , "pies":"szczeka"}
+print(dictionary1)
+
+#wypisywanie zmiennych
+liczba3 = 175
+
+print(liczba3)
+print(5+7)
+
+print(True)
+#czytanie danych od użytkownika
+
+liczba4 = input("podaj liczbę")
+print(liczba4)
+
+
+
